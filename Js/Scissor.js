610bea2f0c016ef7a838c5d03be01e4460bfdc67
Scott <scottunogundersen@gmail.com>
2023-08-30 11:11:48 +0200

    Counter that works

--- Js/Scissor.js
+++ Js/Scissor.js
@@ -1,8 +1,17 @@
 
-let R=Rock
-let S=Scissor
-let P=Paper
+const start = document.querySelector(".start")
+const buttons = document.getElementById("interface")
+const counter = document.querySelector(".counter")
+start.addEventListener("click", (e) => {
+    start.style.display = "none"
+    buttons.style.display = "block"
+    let i = 3
+    timer = setInterval(function(){
+        if (i <= 0) {
+            clearInterval(timer)
+        }
+        counter.innerHTML = i
+        i-=1
+    },1000)
+})
 
-R>S
-S>P
-P>S
